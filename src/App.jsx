1e3ce0d0eff7078feeed98eaf6adec6211152852
philopateer reda <filopateerredakamel2004@gmail.com>
2024-08-28 13:45:18 +0300

    seperate the data of the todo so I can manipulate its data

--- src/App.jsx
+++ src/App.jsx
@@ -4,54 +4,69 @@ import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
-  const [T, setT] = useState("a");
+  const [inputValue, setInputValue] = useState("");
 
-  const inputT = (event) => {
-    setT(event.target.value);
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
   };
 
-  const addtodo = () => {
-    const todo = [
-      <label>
-        <input type="checkbox" className="checkB" />
-        {T}
-      </label>,
-    ];
-    setTodos((prevT) => prevT.concat(todo));
+  const addTodo = () => {
+    if (inputValue.trim() !== "") {
+      const newTodo = {
+        id: Date.now(),
+        text: inputValue,
+        completed: false
+      };
+      setTodos(prevTodos => [...prevTodos, newTodo]);
+      setInputValue("");
+    }
   };
+
+  const toggleTodo = (id) => {
+    setTodos(prevTodos =>
+      prevTodos.map(todo =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const actualTodos=todos.map(todo => (
+    <label key={todo.id}>
+      <input
+        type="checkbox"
+        className="checkB"
+        checked={todo.completed}
+        onChange={() => toggleTodo(todo.id)}
+      />
+      {todo.text}
+    </label>
+  ))
 
   return (
-    <>
-      <div className="main">
-        <div className="todos">
-          {todos}
-
-          <label>
-            <input type="checkbox" className="checkB" />
-            Option 1
-          </label>
-        </div>
-
-        <div className="neck"></div>
-
-        <div className="input">
-          <input
-            type="text"
-            placeholder="add your todos"
-            id="I"
-            value={T}
-            onChange={inputT}
-          ></input>
-          <button id="sub" onClick={addtodo}>
-            add
-          </button>
-        </div>
-
-        <div className="table">
-          <img src={wood} alt="" id="img" />
-        </div>
+    <div className="main">
+      <div className="todos">
+        {actualTodos}
       </div>
-    </>
+
+      <div className="neck"></div>
+
+      <div className="input">
+        <input
+          type="text"
+          placeholder="add your todos"
+          id="I"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button id="sub" onClick={addTodo}>
+          add
+        </button>
+      </div>
+
+      <div className="table">
+        <img src={wood} alt="Wooden background" id="img" />
+      </div>
+    </div>
   );
 }
 
